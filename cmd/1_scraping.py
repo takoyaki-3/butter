d9639a2cc091310a71bf7a9fecb82446214bfea1
takoyaki-3 <mail@takoyaki3.com>
2023-04-11 12:14:47 +0900

    Enabled Python to run in a container.

--- cmd/1_scraping.py
+++ cmd/1_scraping.py
@@ -5,6 +5,8 @@ import time
 
 options = Options()
 options.headless = True
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 driver.get("https://gtfs-data.jp/search")
 time.sleep(5)  # JavaScriptが読み込まれるのを待つ
