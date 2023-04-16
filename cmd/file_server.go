0ff21c2ca46d5f773800dd3523e26e6acba88368
takoyaki-3 <mail@takoyaki3.com>
2023-04-16 20:37:04 +0900

    fix CROS origin

--- cmd/file_server.go
+++ cmd/file_server.go
@@ -12,6 +12,26 @@ func main() {
 	fs := http.FileServer(http.Dir(dir))
 	http.Handle("/", fs)
 
+	// CORS設定
+	headers := make(http.Header)
+	headers.Set("Access-Control-Allow-Origin", "*")
+	headers.Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	headers.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+	corsHandler := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			for key, values := range headers {
+				for _, value := range values {
+						w.Header().Add(key, value)
+				}
+			}
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+			h.ServeHTTP(w, r)
+		})
+	}
+	http.ListenAndServe(":"+port, corsHandler(http.DefaultServeMux))
 	log.Printf("Serving %s on HTTP port: %s\n", dir, port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
