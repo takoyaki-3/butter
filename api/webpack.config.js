c956d43261f2e5b83b305ec9d17ba504023e980d
takoyaki-3 <mail@takoyaki3.com>
2023-04-27 09:12:11 +0900

    Added code to download and extract .tar.gz

---
+++ api/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'worker.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  target: 'webworker'
+};
