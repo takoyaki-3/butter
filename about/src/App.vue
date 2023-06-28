fdd9902e548f93a358718df29d1db6686188a9d8
takoyaki-3 <mail@takoyaki3.com>
2023-06-28 18:26:38 +0900

    フォントの変更等

--- about/src/App.vue
+++ about/src/App.vue
@@ -15,14 +15,14 @@
           width="40"
         /> -->
 
-        <v-img
+        <!-- <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
       </div>
 
       <v-spacer></v-spacer>
