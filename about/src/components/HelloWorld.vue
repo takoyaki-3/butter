9f4ff36b4ebc15dbebfa280adec7df753e63a06a
takoyaki-3 <mail@takoyaki3.com>
2023-06-30 15:55:52 +0900

    Added map. bad layout

--- about/src/components/HelloWorld.vue
+++ about/src/components/HelloWorld.vue
@@ -41,48 +41,6 @@
 
     <DemoSpace/>
     <SampleCode/>
-
-    <v-row>
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-
-        <v-row justify="left">
-          <p></p>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="left">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
