ddb57aadf253279af3815df690e8225b0bb2ccc3
takoyaki-3 <mail@takoyaki3.com>
2023-08-14 11:33:32 +0900

    プライベート仕様に変更

--- src/components/DemoSpace.vue
+++ src/components/DemoSpace.vue
@@ -95,7 +95,7 @@
 import {latLng,Icon} from 'leaflet';
 import { LMap,LTileLayer,LMarker } from "vue2-leaflet";
 import Butter from 'butter-lib/dist.js';
-await Butter.init()
+await Butter.init("https://butter-private.app.takoyaki3.com/root.json")
 import 'leaflet/dist/leaflet.css'
 
 export default {
