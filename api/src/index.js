b20a20feaa7f741a38f04476e17674f7a5b10fc9
takoyaki-3 <mail@takoyaki3.com>
2024-01-02 10:17:45 +0900

    v1.0.19

--- api/src/index.js
+++ api/src/index.js
@@ -1,65 +1,58 @@
-import Butter from 'butter-lib/dist.js'
+import Butter from '../../lib/dist'
 
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
-  const urlParams = new URL(request.url).searchParams;
-
-  // リクエストパラメータから取得
-  const method = urlParams.get('method');
-
-  // 
-  const gtfsID = urlParams.get('gtfsID');
-  const options = urlParams.get('options');
-  const lat = urlParams.get('lat');
-  const lon = urlParams.get('lon');
-  const radius = urlParams.get('radius');
-  const name = urlParams.get('name');
-
-  Butter.init()
-
-  if (method == 'fetchTimeTableV1') {
-    const tt = await Butter.fetchTimeTableV1(gtfsID, JSON.parse(options))
-    return new Response(JSON.stringify(tt), {
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-    })
-  } else if (method == 'fetchStopsV1') {
-    const tt = await Butter.fetchStopsV1(lat, lon, radius, name)
-    return new Response(JSON.stringify(tt), {
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-    })
-  } else if (method == 'dataListV1') {
-    const tt = await Butter.getHostDataList()
-    return new Response(JSON.stringify({ data_list: tt }), {
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      },
-    })
+  if (request.method !== 'POST') {
+    return new Response('Only POST requests are allowed', { status: 405 });
   }
 
-  return new Response("hello, butter !", {
-    headers: {
-      'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-  })
+  try {
+    // リクエストのJSONボディを解析
+    const { functionName, params } = await request.json();
+
+    // // const response = await fetch('https://butter.takoyaki3.com/v0.0.0/root.json'); // ここにデータを取得したいURLを入れます
+    // const response = await fetch('https://butter.oozora283.com/ToeiBus/info.json'); // ここにデータを取得したいURLを入れます
+    // if (!response.ok) {
+    //   console.log({json:JSON.stringify(response)});
+    //   throw new Error('ネットワークレスポンスが不正です。');
+    // }
+    // const data = await response.json();
+    // console.log(data); // ここで取得したデータを扱います
+    // return new Response(JSON.stringify(data), {
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
+
+    // BuTTERライブラリの初期化
+    Butter.init('https://butter.takoyaki3.com/v0.0.0/root.json',{useFetch: true});
+    console.log(Butter);
+
+    // 指定されたBuTTER関数の実行
+    if (functionName in Butter) {
+      const result = await Butter[functionName](...params);
+      return new Response(JSON.stringify(result), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } else {
+      return new Response(JSON.stringify({ error: 'Function not found' }), {
+        status: 400,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
+  }
 }
 
 // https://butter.hatano-yuuta7921.workers.dev/?method=fetchTimeTableV1&gtfsID=ToeiBus&options={%22date%22:%2220240320%22,%22stop_ids%22:[%220605-07%22]}
